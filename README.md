61351274d83534f6e98bc0e0d236b899ffeb1b90
Nick <90505328+napswastaken@users.noreply.github.com>
2023-08-18 16:54:03 +1200

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
-# TraderPlus-FileDiffLogger
+<h3 align="center">TraderPlus FileDiffLogger</h3>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- ABOUT THE PROJECT -->
+
+
+
+
+
+
+
+## About The Project
+
+
+
+<h3 align="center">Overview:</h3> 
+This Python script serves the purpose of comparing the contents of two files: TraderPlusPriceConfig.json and Types.xml.
+It generates two separate logs that highlight discrepancies between the two files. These logs are saved in the \downloads directory.
+
+
+ <br />1. missing_in_types_log.txt: This log documents items that are absent in Types.xml but are present in TraderPlusPriceConfig.json.
+ <br />2. missing_in_trader_log.txt: This log records items that are absent in TraderPlusPriceConfig.json but exist in Types.xml.
+
+Additionally, the script attempts to create placeholder versions of types.xml and \TraderPlusPriceConfig.json. 
+These placeholder files can be used as templates for incorporating actual code. However, it's essential to edit these files before using them. 
+They shouldn't be directly copied and pasted into projects without proper modifications.
+
+The purpose of this script is to facilitate the identification of inconsistencies between the two mentioned files and to help create basic template files and prevent items spawning which can't be sold, or trader items being sold which aren't in types.
+
+
+<h3 align="center">License:</h3> 
+General Public License v3.0 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+
+
+
+
+
+
+
+
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Types__Trader_Compare.py
@@ -0,0 +1,65 @@
+'''
+This Python script serves the purpose of comparing the contents of two files: TraderPlusPriceConfig.json and Types.xml.
+It generates two separate logs that highlight discrepancies between the two files. These logs are saved in the \downloads directory.
+
+1. missing_in_types_log.txt: This log documents items that are absent in Types.xml but are present in TraderPlusPriceConfig.json.
+2. missing_in_trader_log.txt: This log records items that are absent in TraderPlusPriceConfig.json but exist in Types.xml.
+
+The purpose of this script is to facilitate the identification of inconsistencies between the two mentioned files.
+
+Please adjust the file paths and other configurations as needed before running the script.
+''' 
+__author__ = "naps"
+__copyright__ = "Copyright (C) 2023 Nick Shepherd"
+__license__ = "General Public License v3.0"
+__version__ = "1.0"
+
+import json
+import xml.etree.ElementTree as ET
+import os
+
+
+with open(r'Path-to-your-TraderPlusPriceConfig', 'r') as json_file: # Point towards your TraderPlusPriceConfig.json
+    trader_data = json.load(json_file)
+
+xml_path = r'Path-to-your-Types' # Point towards your types.xml
+tree = ET.parse(xml_path)
+root = tree.getroot()
+
+json_items = set()
+for category in trader_data['TraderCategories']:
+    for product in category['Products']:
+        item_name = product.split(',')[0]
+        json_items.add(item_name)
+
+xml_items = set()
+for item_type in root.findall('type'):
+    item_name = item_type.attrib['name']
+    xml_items.add(item_name)
+
+missing_in_xml = set()
+for item in json_items:
+    xml_item = root.find(f"./type[@name='{item}']")
+    if xml_item is None or (xml_item.find('nominal') is not None and int(xml_item.find('nominal').text) < 1):
+        missing_in_xml.add(item)
+
+missing_in_json = xml_items - json_items
+
+missing_in_xml_content = "--------------------------------------\nClasses missing in types.xml but are found in TraderPlusPriceConfig.json\n--------------------------------------\n\n" + "\n".join(missing_in_xml) if missing_in_xml else "No items missing in types.xml."
+
+missing_in_json_content = "--------------------------------------\nClasses missing in TraderPlusPriceConfig.json but are found in types.xml\n--------------------------------------\n\n" + "\n".join(missing_in_json) if missing_in_json else "No items missing in TraderPlusPriceConfig.json."
+
+log_folder = os.path.join(os.path.expanduser('~'), 'Downloads\\Export')
+os.makedirs(log_folder, exist_ok=True)
+
+log_xml_path = os.path.join(log_folder, 'missing_in_types.txt')
+log_json_path = os.path.join(log_folder, 'missing_in_trader.txt')
+
+with open(log_xml_path, 'w') as log_xml_file:
+    log_xml_file.write(missing_in_xml_content)
+
+with open(log_json_path, 'w') as log_json_file:
+    log_json_file.write(missing_in_json_content)
+
+print(f"Missing items log (XML) has been saved to: {log_xml_path}")
+print(f"Missing items log (JSON) has been saved to: {log_json_path}")
