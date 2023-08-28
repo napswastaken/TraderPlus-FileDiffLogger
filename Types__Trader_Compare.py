893742495193a829e47a6d915e27e19f96f094f2
Nick <90505328+napswastaken@users.noreply.github.com>
2023-08-28 18:28:23 +1200

    convert to lowercase to avoid issues

--- Types__Trader_Compare.py
+++ Types__Trader_Compare.py
@@ -1,4 +1,4 @@
-'''
+"""
 This Python script serves the purpose of comparing the contents of two files: TraderPlusPriceConfig.json and Types.xml.
 It generates two separate logs that highlight discrepancies between the two files. These logs are saved in the \downloads directory.
 
@@ -8,7 +8,7 @@ It generates two separate logs that highlight discrepancies between the two file
 The purpose of this script is to facilitate the identification of inconsistencies between the two mentioned files.
 
 Please adjust the file paths and other configurations as needed before running the script.
-''' 
+"""
 __author__ = "naps"
 __copyright__ = "Copyright (C) 2023 Nick Shepherd"
 __license__ = "General Public License v3.0"
@@ -19,7 +19,9 @@ import xml.etree.ElementTree as ET
 import os
 
 # Point towards your TraderPlusPriceConfig.json
-with open(r'Path-to-your-TraderPlusPriceConfig', 'r') as json_file:
+with open(
+        r'Path-to-your-TraderPlusPriceConfig',
+        'r') as json_file:
     trader_data = json.load(json_file)
 
 # Point towards your types.xml
@@ -30,12 +32,12 @@ root = tree.getroot()
 json_items = set()
 for category in trader_data['TraderCategories']:
     for product in category['Products']:
-        item_name = product.split(',')[0]
+        item_name = product.split(',')[0].lower()
         json_items.add(item_name)
 
 xml_items = set()
 for item_type in root.findall('type'):
-    item_name = item_type.attrib['name']
+    item_name = item_type.attrib['name'].lower()
     nominal = item_type.find('nominal')
     if nominal is not None and int(nominal.text) >= 1:
         xml_items.add(item_name)
@@ -48,11 +50,13 @@ for item in json_items:
 
 missing_in_json = xml_items - json_items
 
-missing_in_xml_content = "----------------------------------------------------------------------------\nClasses missing in types.xml but are found in TraderPlusPriceConfig.json\n----------------------------------------------------------------------------\n\n" + "\n".join(missing_in_xml) if missing_in_xml else "No items missing in types.xml."
+missing_in_xml_content = "----------------------------------------------------------------------------\nClasses missing in types.xml but are found in TraderPlusPriceConfig.json\n----------------------------------------------------------------------------\n\n" + "\n".join(
+    missing_in_xml) if missing_in_xml else "No items missing in types.xml."
 
-missing_in_json_content = "----------------------------------------------------------------------------\nClasses missing in TraderPlusPriceConfig.json but are found in types.xml AND which has a nominal value =>1 \n----------------------------------------------------------------------------\n\n" + "\n".join(missing_in_json) if missing_in_json else "No items missing in TraderPlusPriceConfig.json."
+missing_in_json_content = "----------------------------------------------------------------------------\nClasses missing in TraderPlusPriceConfig.json but are found in types.xml AND which has a nominal value => 1 \n----------------------------------------------------------------------------\n\n" + "\n".join(
+    missing_in_json) if missing_in_json else "No items missing in TraderPlusPriceConfig.json."
 
-log_folder = os.path.join(os.path.expanduser('~'), 'Downloads\\Export\\Missing') #Saves to your downloads folder.
+log_folder = os.path.join(os.path.expanduser('~'), 'Downloads\\Export\\Missing')  # Saves to your downloads folder.
 os.makedirs(log_folder, exist_ok=True)
 
 log_xml_path = os.path.join(log_folder, 'missing_in_types.txt')
